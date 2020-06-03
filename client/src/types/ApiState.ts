d5d675cf0f97afa033637b309b14b580b467e382
Zplaninic <zvonimir.planinic@gmail.com>
2020-06-04 00:08:27 +0200

    add data types

---
+++ client/src/types/ApiState.ts
@@ -0,0 +1,31 @@
+interface Init {
+    status: 'init'
+}
+
+interface Loading {
+    status: 'loading'
+}
+
+interface Loaded<T> {
+    status: 'loaded'
+    payload: {
+        data: T
+    }
+}
+
+interface EndData {
+    status: 'endData'
+}
+
+interface Error {
+    status: 'error';
+    error: Error;
+}
+
+
+export type ApiState<T> = 
+    | Init
+    | Loading
+    | Loaded<T>
+    | EndData
+    | Error;
---
+++ client/src/types/UserInterface.ts
@@ -0,0 +1,9 @@
+export interface UserInterface {
+    id: string;
+    name: string;
+    surname: string;
+    address: string;
+    number: string;
+    email: string;
+    birthday: string;
+}
