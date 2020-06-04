fc0fe96c4ae3aa4b20dd81390f96b0646633daeb
Zplaninic <zvonimir.planinic@gmail.com>
2020-06-04 18:50:41 +0200

    add global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import UserList from './components/UserList'
 import CreateUser from './components/CreateUser'
 import {GlobalProvider} from './context/GlobalState'
-import Search from './components/Search'
+import styled from 'styled-components'
+import Header from './components/Header'
 
 
 import './App.css';
@@ -10,11 +11,21 @@ import './App.css';
 const App: React.FC = () => {
   return (
       <GlobalProvider>
-        <UserList />
-        <CreateUser /> 
-        <Search />
+        <AppContainer>
+          <Header />
+          <UserList />
+          <CreateUser /> 
+        </AppContainer>
       </GlobalProvider>
     )
 }
 
 export default App;
+
+const AppContainer = styled.div `
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 50px auto;
+  gap: 20px;
+  height: 100vh;
+`
